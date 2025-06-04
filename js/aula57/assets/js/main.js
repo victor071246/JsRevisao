2533b8c111bd07dfe56be1af3c4ecf152557e0ef
victor071246 <daderiovictorfelipe@gmail.com>
2025-06-03 23:56:07 -0300

    avanço dia 03/06/2025

--- js/aula57/assets/js/main.js
+++ js/aula57/assets/js/main.js
@@ -1,19 +1,75 @@
 function criaCalculadora() {
 	return {
-		display: document.querySelector(),
+		display: document.querySelector('.display'),
+		btnClear: document.querySelector('.btn-clear'),
+
+		clearDisplay() {
+			this.display.value = '';
+		},
+
+		apagaUm() {
+			this.display.value = this.display.value.slice(0, -1);
+		},
 
 		inicia() {
-			this.cliqueBotoes;
+			this.cliqueBotoes();
+			this.clearDisplay();
+			this.pressionaEnter();
+		},
+
+		pressionaEnter() {
+			this.display.addEventListener('keyup', (e) => {
+				if (e.keyCode === 13) {
+					this.realizaConta();
+				}
+			});
+		},
+
+		realizaConta() {
+			let conta = this.display.value;
+
+			try {
+				conta = eval(conta);
+
+				if (!conta) {
+					alert('Conta inválida');
+					return;
+				}
+
+				this.display.value = String(conta);
+			} catch (e) {
+				alert('Conta inválida');
+				return;
+			}
 		},
 
 		cliqueBotoes() {
-			document.addEventListener('click', function (e) {
-				const el = e.target;
+			document.addEventListener(
+				'click',
+				function (e) {
+					const el = e.target;
 
-				if (e.classList.contains('btn-num')) {
-					this.btnParaDisplay();
-				}
-			});
+					if (el.classList.contains('btn-num')) {
+						this.btnParaDisplay(el.innerText);
+					}
+
+					if (el.classList.contains('btn-clear')) {
+						this.clearDisplay();
+					}
+
+					if (el.classList.contains('btn-del')) {
+						this.apagaUm();
+					}
+
+					if (el.classList.contains('btn-eq')) {
+						this.realizaConta();
+					}
+				}.bind(this)
+			);
+		},
+
+		btnParaDisplay(valor) {
+			this.display.value += valor;
 		},
 	};
 }
