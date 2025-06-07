e831099da3365bab5e49194945fc9a6d42e843d2
victor071246 <daderiovictorfelipe@gmail.com>
2025-06-06 23:30:28 -0300

    progresso dias 06 de Junho de 2025

--- js/aula73/main.js
+++ js/aula73/main.js
@@ -1,4 +1,5 @@
-const produto = { nome: 'Produto', preco: 1.8 };
+const produto = { nome: 'Produto', preco: 1.8, material: 'porcenala' };
 
-for (let entry of Object.entries(produto)) {
+for (let valor of Object.entries(produto)) {
+	console.log(valor[0], valor[1]);
 }
