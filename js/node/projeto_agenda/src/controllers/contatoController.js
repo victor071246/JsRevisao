e4d3632c42fcbb043136795ec6adf4f73ac7b16d
victor071246 <daderiovictorfelipe@gmail.com>
2025-06-15 23:43:47 -0300

    treino de apis dia 15/06/2025 | API com mongoDB finalizada

--- js/node/projeto_agenda/src/controllers/contatoController.js
+++ js/node/projeto_agenda/src/controllers/contatoController.js
@@ -61,3 +61,13 @@ exports.edit = async function (req, res) {
 		console.log(e);
 	}
 };
+
+exports.delete = async function (req, res) {
+	if (!req.params.id) return res.render('404');
+
+	const contato = await Contato.delete(req.params.id);
+	if (!contato) return res.render('404');
+
+	req.flash('success', 'Contato apagado com sucesso');
+	req.session.save(() => res.redirect(`/`));
+};
